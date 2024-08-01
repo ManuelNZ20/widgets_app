003b82b988b17032c6af7d280da7eafd2d46b739
Manuel Walter Navarro Zeta <manuel08n@gmail.com>
2024-07-31 21:40:42 -0500

    implements theme

---
+++ lib/config/constants/color_list_theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const colorList = <Color>[
+  Colors.blue,
+  Colors.teal,
+  Colors.green,
+  Colors.red,
+  Colors.purple,
+  Colors.deepPurple,
+  Colors.orange,
+  Colors.pink,
+];
---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import '../constants/color_list_theme.dart';
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({
+    this.selectedColor = 0,
+  })  : assert(
+          selectedColor >= 0,
+          'Selected Color must be greater then 0',
+        ),
+        assert(
+          selectedColor < colorList.length,
+          'Selected color must be less or equal than ${colorList.length - 1}',
+        );
+
+  ThemeData themeData() => ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: colorList[selectedColor],
+      );
+}
