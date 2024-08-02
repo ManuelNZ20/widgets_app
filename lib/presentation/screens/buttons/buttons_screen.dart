44da63f28c4b1fc91172587ab08284c27ce8623a
Manuel Walter Navarro Zeta <manuel08n@gmail.com>
2024-08-02 17:41:07 -0500

    buttons

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import '../../widgets/widgets.dart';
 
 class ButtonsScreen extends StatelessWidget {
   static const String name = 'buttons';
@@ -10,7 +13,98 @@ class ButtonsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Buttons Screens'),
       ),
-      body: const Placeholder(),
+      body: const _ButtonsView(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => context.pop(),
+        child: const Icon(Icons.arrow_back_ios_rounded),
+      ),
+    );
+  }
+}
+
+class _ButtonsView extends StatelessWidget {
+  const _ButtonsView();
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+        child: Wrap(
+          spacing: 10,
+          alignment: WrapAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Elevated'),
+            ),
+            const ElevatedButton(
+              onPressed: null,
+              child: Text('Elevated Disable'),
+            ),
+            ElevatedButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.access_alarm_rounded),
+              label: const Text('Elevated Icon'),
+            ),
+            FilledButton(
+              onPressed: () {},
+              child: const Text('Filled'),
+            ),
+            FilledButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.flutter_dash),
+              label: const Text('Filled Icon'),
+            ),
+            FilledButton.tonal(
+              onPressed: () {},
+              child: const Text('Filled Tonal'),
+            ),
+            FilledButton.tonalIcon(
+              onPressed: () {},
+              icon: const Icon(Icons.flutter_dash),
+              label: const Text('Filled Tonal Icon'),
+            ),
+            OutlinedButton(
+              onPressed: () {},
+              child: const Text('Outline'),
+            ),
+            OutlinedButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.terminal_rounded),
+              label: const Text('Outline Icon'),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: const Text('Text'),
+            ),
+            TextButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.flutter_dash),
+              label: const Text('Text icon'),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.app_registration_rounded),
+            ),
+            IconButton.filled(
+              onPressed: () {},
+              icon: const Icon(Icons.dashboard_customize),
+            ),
+            IconButton.filledTonal(
+              onPressed: () {},
+              icon: const Icon(Icons.workspaces_rounded),
+            ),
+            IconButton.outlined(
+              onPressed: () {},
+              icon: const Icon(Icons.flutter_dash),
+            ),
+            // TODO: Custom Button
+            const CustomButton(),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/presentation/widgets/widgets.dart
+++ lib/presentation/widgets/widgets.dart
@@ -1,1 +1,2 @@
 export '../widgets/list_tile_menu_item.dart';
+export '../widgets/custom_button.dart';
